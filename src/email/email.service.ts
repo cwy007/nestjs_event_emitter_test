8aab4786e94a6a9f1105a03127a3c8119408f9db
chanweiyan <chanweiyan007@gmail.com>
2026-05-12 11:11:47 +0800

    fix: implement email module

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Transporter, createTransport } from 'nodemailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+
+  transporter: Transporter;
+
+  constructor() {
+    this.transporter = createTransport({
+      host: 'smtp.qq.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: '1561931060@qq.com',
+        pass: 'cnwobnqyvsrbgjfa',
+      },
+    });
+  }
+
+  async sendEmail(to: string, subject: string, html: string) {
+    const mailOptions = {
+      from: {
+        name: '系统邮件',
+        address: '1561931060@qq.com',
+      },
+      to,
+      subject,
+      html,
+    };
+    await this.transporter.sendMail(mailOptions);
+  }
+}
