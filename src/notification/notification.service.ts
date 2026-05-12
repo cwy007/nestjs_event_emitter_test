61f31e03f9c1eb9c451b161f7a1be1258ae9589c
chanweiyan <chanweiyan007@gmail.com>
2026-05-12 11:18:04 +0800

    fix: test user login and send notification email scenario

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { EmailService } from 'src/email/email.service';
+import { OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class NotificationService {
+
+  constructor(private readonly emailService: EmailService) { }
+
+  @OnEvent('user.register')
+  async handleUserRegisterEvent(payload: any) {
+    console.log('Received user.register event with payload:', payload);
+    await this.emailService.sendEmail(
+      payload.email,
+      'Welcome to our service',
+      `<p>Dear ${payload.username}, welcome to our service!</p>`,
+    );
+  }
+
+
+
   create(createNotificationDto: CreateNotificationDto) {
     return 'This action adds a new notification';
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class UserService {
+
+  @Inject(EventEmitter2)
+  private eventEmitter: EventEmitter2;
+
   create(createUserDto: CreateUserDto) {
+    this.eventEmitter.emit('user.register', {
+      username: createUserDto.username,
+      email: createUserDto.email,
+    });
     return 'This action adds a new user';
   }
 
